15d2c47408181d3be45ab97a0550c6a64268bd5f
hanhtd161 <hanhtd161@gmail.com>
2021-02-01 00:47:27 +0700

    formatex3

--- javaproject/src/ex3/bt1.java
+++ javaproject/src/ex3/bt1.java
@@ -44,48 +44,50 @@ public class bt1 {
         }
         System.out.println("");
         System.out.println("Trong mang co " + even + " so chan");
-        System.out.print("Cac so nguyen to trong mang:"+"\t");
+        System.out.print("Cac so nguyen to trong mang:" + "\t");
         for (int i = 0; i < n; i++) {
             int z = 0;
-            if (arr[i]<2){z=1;}
-            for (int j=2;j<= Math.sqrt(arr[i]);j++){
+            if (arr[i] < 2) {
+                z = 1;
+            }
+            for (int j = 2; j <= Math.sqrt(arr[i]); j++) {
                 if (arr[i] % j == 0) {
                     z = 1;
                     break;
                 }
             }
             if (z == 0) {
-                System.out.print(arr[i]+"\t");
+                System.out.print(arr[i] + "\t");
             }
         }
         System.out.println("");
-        int temp=0;
-        for (int i=0;i<n-1;i++){
-            for (int j=i+1;j<n;j++){
-                if (arr[i]>arr[j]){
+        int temp = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
                     temp = arr[i];
-                    arr[i]=arr[j];
-                    arr[j]= temp;
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
         System.out.println("Mang theo chieu tang dan");
-        for(int i=0;i<n;i++){
-        System.out.print(arr[i] + "\t");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + "\t");
         }
         System.out.println("");
-        for (int i=0;i<n-1;i++){
-            for (int j=i+1;j<n;j++){
-                if (arr[i]<arr[j]){
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] < arr[j]) {
                     temp = arr[i];
-                    arr[i]=arr[j];
-                    arr[j]= temp;
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
         System.out.println("Mang theo chieu giam dan");
-        for(int i=0;i<n;i++){
-        System.out.print(arr[i] + "\t");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + "\t");
         }
 
     }
