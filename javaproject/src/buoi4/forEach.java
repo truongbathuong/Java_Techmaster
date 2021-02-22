1cd72d036ba431d8c400980d88d57c969895741a
hanhtd161 <hanhtd161@gmail.com>
2021-02-22 21:49:58 +0700

    buoi4

---
+++ javaproject/src/buoi4/forEach.java
@@ -0,0 +1,10 @@
+package buoi4;
+
+public class forEach {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        for (int a:arr){
+            System.out.println(a);
+        }
+    }
+}
